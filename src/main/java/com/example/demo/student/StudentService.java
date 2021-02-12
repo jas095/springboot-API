1fe6fb614883c4b65822fac425d7228df14873aa
jas <thejas095@gmail.com>
2021-02-12 19:24:13 -0400

    Rule applied to email. Business Logic for new Student

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,5 +18,14 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("This email has been used before");
+        }
+        studentRepository.save(student);
     }
 }
